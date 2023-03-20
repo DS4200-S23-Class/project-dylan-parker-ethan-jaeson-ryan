75c0a120c410d1b918d12f24b1c27c09312cd1c9
ejwaple <122317503+ejwaple@users.noreply.github.com>
2023-03-20 15:47:41 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-var margin = {top: 10, right: 30, bottom: 30, left: 60},
+const margin = {top: 10, right: 30, bottom: 30, left: 60},
     width = 460 - margin.left - margin.right,
     height = 400 - margin.top - margin.bottom;
 
-var svg = d3.select("#left")
+const svg = d3.select("#left")
   .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
@@ -12,20 +12,20 @@ var svg = d3.select("#left")
 
 d3.csv("https://raw.githubusercontent.com/DS4200-S23-Class/hw-06-ethan-nick/master/data/iris.csv", function(data) {
 
-  var x = d3.scaleLinear()
+  const x = d3.scaleLinear()
     .domain([0, 8])
     .range([ 0, width ]);
   svg.append("g")
     .attr("transform", "translate(0," + height + ")")
     .call(d3.axisBottom(x));
 
-  var y = d3.scaleLinear()
+  const y = d3.scaleLinear()
     .domain([0, 7.0])
     .range([ height, 0]);
   svg.append("g")
     .call(d3.axisLeft(y));
 
-  var circles = svg.append('g')
+  const circles = svg.append('g')
   	.attr("id", "plane")
     .selectAll("point")
     .data(data)
